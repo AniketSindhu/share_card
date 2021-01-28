352fea368c74792d2218e5e84ec99c1c90ac9a72
Aniket sindhu <sindhuaniket@gmail.com>
2021-01-28 21:29:48 +0530

    phone fixed

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -18,9 +18,10 @@ class _LoginState extends State<Login> {
   String phone;
   String country;
   @override
-  void initState(){
+  void initState() {
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,15 +30,14 @@ class _LoginState extends State<Login> {
         (context.percentHeight * 6).heightBox,
         "sign up".tr().toString().text.bold.size(35).makeCentered(),
         25.heightBox,
-        Image.asset('assets/login.jpg',height: context.percentHeight*40,),
+        Image.asset(
+          'assets/login.jpg',
+          height: context.percentHeight * 40,
+        ),
         30.heightBox,
         "sign_phone".tr().toString().text.semiBold.size(20).make(),
         8.heightBox,
-        "valid_country".tr().toString()
-            .text
-            .thin
-            .size(15)
-            .make(),
+        "valid_country".tr().toString().text.thin.size(15).make(),
         10.heightBox,
         IntlPhoneField(
           decoration: InputDecoration(
@@ -59,7 +59,7 @@ class _LoginState extends State<Login> {
         FlatButton(
           onPressed: () async {
             final close = context.showLoading(msg: 'loading'.tr().toString());
-            Future.delayed(Duration(seconds: 5),close);
+            Future.delayed(Duration(seconds: 5), close);
             await FirebaseAuth.instance.verifyPhoneNumber(
               phoneNumber: phone,
               verificationCompleted: (PhoneAuthCredential credential) async {
@@ -76,7 +76,7 @@ class _LoginState extends State<Login> {
                 prefs.setBool('login', true);
                 await firstTime().then((value) async {
                   if (value) {
-                    await addUser(phone,country).then((value) {
+                    await addUser(phone, country).then((value) {
                       Navigator.pushReplacement(context,
                           MaterialPageRoute(builder: (context) {
                         return HomePage();
@@ -102,7 +102,7 @@ class _LoginState extends State<Login> {
                       pdVertical: 10);
                 } else {
                   context.showToast(
-                      msg: 'invalid_phone'.tr().toString(),
+                      msg: '${e.code}'.toString(),
                       showTime: 4500,
                       bgColor: Vx.red500,
                       textColor: Colors.white,
@@ -116,24 +116,28 @@ class _LoginState extends State<Login> {
                     context,
                     MaterialPageRoute(
                         builder: (context) => OTP(
-                              verificationId: verificationId,
-                              phoneNo: phone,
-                              country:country
-                            )));
+                            verificationId: verificationId,
+                            phoneNo: phone,
+                            country: country)));
               },
               codeAutoRetrievalTimeout: (String verificationId) {},
             );
           },
-          child: "continue".tr().toString().text.size(22).semiBold.white.make().py12(),
+          child: "continue"
+              .tr()
+              .toString()
+              .text
+              .size(22)
+              .semiBold
+              .white
+              .make()
+              .py12(),
           color: Colors.blue,
           shape:
               RoundedRectangleBorder(borderRadius: BorderRadius.circular(45)),
         ).w(double.infinity).centered(),
         4.heightBox,
-        "t&c".tr().toString()
-            .text
-            .size(15)
-            .makeCentered(),
+        "t&c".tr().toString().text.size(15).makeCentered(),
       ],
       alignment: MainAxisAlignment.center,
     ).p20().centered().scrollVertical());
